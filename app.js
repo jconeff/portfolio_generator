3d2c5068b32d1813f5385188e0b5d05041a60c71
jconeff <jessicaconeff@gmail.com>
2020-12-19 22:47:47 -0500

    updated tempelate

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
-// const generatePage = require('./src/page-template.js');
-
-
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -14,7 +12,7 @@ const promptUser = () => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log("Please enter your name!");
+                    console.log('Please enter your name!');
                     return false;
                 }
             }
@@ -22,26 +20,26 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub username',
-            validate: userInput => {
-                if (userInput) {
+            message: 'Enter your Github Username. (Required)',
+            validate: githubInput => {
+                if (githubInput) {
                     return true;
                 } else {
-                    console.log("Please enter your GitHub username!");
+                    console.log('Please enter your username!');
                     return false;
                 }
             }
-        },
+        }, 
         {
-            type: 'confirm',
+            type: 'confirm', 
             name: 'confirmAbout',
             message: 'Would you like to enter some information about yourself for an "About" section?',
-            default: true
+            default: true,
         },
-        {
+        { 
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:',
+            message: 'Provide some info about yourself:',
             when: ({ confirmAbout }) => {
                 if (confirmAbout) {
                     return true;
@@ -50,43 +48,36 @@ const promptUser = () => {
                 }
             }
         }
-    ]);
-};
+    ])
+}
 
 const promptProject = portfolioData => {
+
+    // If there is no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
     console.log(`
 =================
 Add a New Project
 =================
-`);
-
-    // If there's no 'projects' array property, create one
-    if (!portfolioData.projects) {
-        portfolioData.projects = [];
-    }
+    `);
     return inquirer.prompt([
         {
-          type: 'input',
-          name: 'name',
-          message: 'What is the name of your project? (Required)',
-          validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('Please enter a name for your project!');
-              return false;
-            }
-          }
-        },
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        }, 
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of your project. (Required)',
+            message: 'Provide a description of the project. (Required)',
             validate: descriptionInput => {
                 if (descriptionInput) {
                     return true;
                 } else {
-                    console.log("Please provide a description of your project!");
+                    console.log('Please enter a description!');
                     return false;
                 }
             }
@@ -94,29 +85,29 @@ Add a New Project
         {
             type: 'checkbox',
             name: 'languages',
-            message: 'What did you build this project with? (Check all that apply)',
+            message: 'What did you build this project with?  (Check all that apply)',
             choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
         },
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)',
+            message: 'Enter the GitHub link to your project.  (Required)',
             validate: linkInput => {
                 if (linkInput) {
                     return true;
                 } else {
-                    console.log("Please enter the GitHub link to your project!");
+                    console.log('Please enter a Github link!');
                     return false;
                 }
             }
         },
         {
-            type: 'confirm',
+            type: 'confirm', 
             name: 'feature',
             message: 'Would you like to feature this project?',
             default: false
-        },
-        {
+        }, 
+        { 
             type: 'confirm',
             name: 'confirmAddProject',
             message: 'Would you like to enter another project?',
@@ -125,16 +116,72 @@ Add a New Project
     ])
     .then(projectData => {
         portfolioData.projects.push(projectData);
+
         if (projectData.confirmAddProject) {
             return promptProject(portfolioData);
         } else {
             return portfolioData;
         }
-    });
+    })
 }
 
 promptUser()
-    .then(promptProject)
+//     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        // const pageHTML = generatePage(portfolioData);
+
+        const pageHTML = generatePage(mockData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete!  Check out index.html to see the output!')
+        });
     });
+
+
+const mockData = {
+    name: 'Lernantino',
+    github: 'lernantino',
+    confirmAbout: true,
+    about:
+      'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+    projects: [
+      {
+        name: 'Run Buddy',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['HTML', 'CSS'],
+        link: 'https://github.com/lernantino/run-buddy',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskinator',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'HTML', 'CSS'],
+        link: 'https://github.com/lernantino/taskinator',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskmaster Pro',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+        link: 'https://github.com/lernantino/taskmaster-pro',
+        feature: false,
+        confirmAddProject: true
+      },
+      {
+        name: 'Robot Gladiators',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+        languages: ['JavaScript'],
+        link: 'https://github.com/lernantino/robot-gladiators',
+        feature: false,
+        confirmAddProject: false
+      }
+    ]
+}
